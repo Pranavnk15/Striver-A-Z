e301cd98ed362cff7c0acd41ef0bd18d16fc093b
Pranav <khandagalepranav1546@gmail.com>
2024-01-14 12:08:50 +0530

    N-Queens

---
+++ Recursion/N-Queens.java
@@ -0,0 +1,89 @@
+// 51. N-Queens
+
+// The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+// Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+
+// Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+
+ 
+
+// Example 1:
+
+
+// Input: n = 4
+// Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+// Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+// Example 2:
+
+// Input: n = 1
+// Output: [["Q"]]
+
+// Approach:- We will use recursion and backtracking to solve this,
+// in this we will try to fill from left to right, and from one column top to down,
+// //In this we will only check the left top diagonal, left straight, left bottom diagonal, 
+// //as because we are placing the queens from left to right, hence we don't need to check the right side, and the top and bottom of the current column.'
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+        List < List < String >> res = new ArrayList < List < String >> ();
+        dfs(0, board, res);
+        return res;
+    }
+
+    static boolean validate(char[][] board, int row, int col) {
+        int duprow = row;
+        int dupcol = col;
+        while (row >= 0 && col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+        while (col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+        while (col >= 0 && row < board.length) {
+            if (board[row][col] == 'Q') return false;
+            col--;
+            row++;
+        }
+        return true;
+    }
+
+    static void dfs(int col, char[][] board, List < List < String >> res) {
+        if (col == board.length) {
+            res.add(construct(board));
+            return;
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            if (validate(board, row, col)) {
+                board[row][col] = 'Q';
+                dfs(col + 1, board, res);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+
+
+    static List < String > construct(char[][] board) {
+        List < String > res = new LinkedList < String > ();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+}
